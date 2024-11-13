0d2024bb1e9c6e7d411577fb08533652ebafcf1f
Beknazar Baktybekov <baktybekovbeknazar4@gmail.com>
2024-11-13 14:49:26 +0600

    CounterPage.js

--- CounterPage.js
+++ CounterPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 const CounterPage = () => {
-    const count = useSelector((state) => state); // Подключение состояния
+    const count = useSelector((state) => state); 
     const dispatch = useDispatch();
 
     const handleIncrement = (value) => {
